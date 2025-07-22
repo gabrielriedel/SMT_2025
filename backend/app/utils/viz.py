876caf2bc214dcd90543ee63e2a0e3040f40bd89
gabrielriedel <62902831+gabrielriedel@users.noreply.github.com>
2025-07-21 21:04:45 -0500

    fix: fix infinite loop

--- backend/app/utils/viz.py
+++ backend/app/utils/viz.py
@@ -5,7 +5,6 @@ import pandas as pd
 def random_play(df):
     random_row = df.sample(n=1).reset_index(drop=True)
     with db.connect("database/smt_2025.db") as con:
-        try:
                 game_string = random_row["game_str"].iloc[0]
                 play_id = random_row["play_per_game"].iloc[0]
                 player_position_df = con.sql(f"""SELECT * FROM player_pos  
@@ -14,5 +13,5 @@ def random_play(df):
                                                 WHERE game_str = '{game_string}';""").df()
                 buf = plot_animation(player_position_df, ball_position_df, int(play_id), True)
                 return buf
-        except:
-             return(random_play(df))
+        # except:
+        #      return(random_play(df))
